2299b995036a3e669188653a0613d3dc8ef0c312
Eidson <eidsond@umsystem.edu>
2018-02-02 23:26:37 -0600

    Refactored Parser.cs
    Cleaned up code and deleted unused statements.
    
    Fixes #1

--- TwoDave.ServerSentEventsParser/Parser.cs
+++ TwoDave.ServerSentEventsParser/Parser.cs
@@ -7,38 +7,9 @@ namespace TwoDave.ServerSentEventsParser
     {
         public static string Parse(string input, out string remainder)
         {
-            // Some common line terminator unicode values:
-            // \u000a - Line Feed
-            // \u000b - Vertical Tab
-            // \u000c - Form Feed
-            // \u000d - Carriage Return
-
-            var lfsearch = input.IndexOf('\u000a'); //first occurence 
-            var crsearch = input.IndexOf('\u000d'); //first occurence
-            var line = "";
-            remainder = "";
-
-            #region Block that counts LFs and CRs - not used
-            //Console.WriteLine("Line Feed first occurence found at index {0}", lfsearch);
-            //Console.WriteLine("Carriage return first occurence found at index {0}", crsearch);
-
-            var lf = 0;
-            var cr = 0;
-            for (var i = 0; i < input.Length; i++)
-            {
-                char temp = input[i];
-
-                if (temp == '\u000a')
-                {
-                    lf++;
-                }
-
-                if (temp == '\u000d')
-                {
-                    cr++;
-                }
-            }
-            #endregion
+            string line = null;
+            var lfsearch = input.IndexOf('\n'); //first occurence 
+            var crsearch = input.IndexOf('\r'); //first occurence
 
             if (lfsearch >= 0 && crsearch >= 0)
             {
@@ -46,18 +17,9 @@ namespace TwoDave.ServerSentEventsParser
                 line = input.Remove(crsearch);
             }
 
-            remainder = input.Remove(0, lfsearch + 1); // lfsearch = -1 when blank. is this good enough?
+            remainder = input.Remove(0, lfsearch + 1);
 
-            if (line == "")
-            {
-                return null;
-            }
-            else
-            {
-                return line;
-            }
-
-            //throw new NotImplementedException();
+            return line;
         }
     }
 }
