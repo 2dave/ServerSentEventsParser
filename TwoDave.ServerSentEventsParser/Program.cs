877e24839d35b2f4e7e90fa2ff8118a1ba1f3f13
David <dave.eidson@gmail.com>
2018-02-21 20:03:48 -0600

    Changed main program to display output of file parsing
    Changed the output of the main program to show all of the elements of a message parsed from a file.

--- TwoDave.ServerSentEventsParser/Program.cs
+++ TwoDave.ServerSentEventsParser/Program.cs
@@ -7,41 +7,37 @@ namespace TwoDave.ServerSentEventsParser
     {
         static void Main(string[] args)
         {
-            #region Debugging and Testing ParseMessage
-            //var input = "data: foo\r\ndata: bar\r\n\r\nevent: next\r\n";
-            //var remainder = "";
-            //SseMessage message = new SseMessage();
-
-            //message = Parser.ParseMessage(input, out remainder);
-
-            //Console.WriteLine("Message data should be:\r\nfoo\r\nbar");
-            //Console.WriteLine(message.Data);
-
-            //Console.WriteLine("Remainder should be:\r\nevent: next\r\n");
-            //Console.WriteLine(remainder);
-            #endregion
-
-            #region Debugging and Testing ParseFile
             var path = @".\data\test.txt";
+
             try
             {
                 var remainder = "";
+
                 SseMessage message = new SseMessage();
-                
+
                 message = Parser.ParseFile(path, out remainder);
 
-                Console.WriteLine("Message data should be:\r\nfoo\r\nbar");
-                Console.WriteLine(message.Data);
+                Console.WriteLine("***Parsing file located at {0} ***", path);
 
-                Console.WriteLine("Remainder should be:\r\nevent: next\r\n");
-                Console.WriteLine(remainder);
+                if (message.Id != null)
+                {
+                    Console.WriteLine("Message ID = {0}", message.Id);
+                }
+                if (message.Event != null)
+                {
+                    Console.WriteLine("Message Event = {0}", message.Event);
+                }
+                if (message.Data != null)
+                {
+                    Console.WriteLine("Message Data = {0}", message.Data);
+                }
+
+                Console.WriteLine("Remainder = {0}", remainder);
             }
             catch (FileNotFoundException e)
             {
                 Console.WriteLine("Could not find: {0}", e.FileName);
             }
-            #endregion
-
         }
     }
 }
